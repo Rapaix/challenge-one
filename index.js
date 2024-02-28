48e8abfd9ee35c05bea124344db25a13917d7d29
Rafael Paixao <rapaix@MacBook-Pro-de-Rafael.local>
2024-02-28 15:59:01 -0300

    add index.js

--- index.js
+++ index.js
@@ -27,3 +27,38 @@ function botaoCopiarTexto() {
   }
   alert("Texto copiado para a área de transferência!");
 }
+
+let cripto = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
+};
+
+let cifra = { enter: "e", imes: "i", ai: "a", ober: "o", ufat: "ufat" };
+function decifrar(texto) {}
+let teste = "gato";
+
+let arrString = teste.split("");
+
+let arrResult = arrString.map((s) => (s in cripto ? cripto[s] : s));
+
+console.log(arrResult.join(""));
+console.log("arrResult");
+console.log();
+
+const decifrar = (searhString, pattern) =>
+  [...searhString.matchAll(new RegExp(pattern, "gi"))].map((a) => a.index);
+
+for (const [key, value] of Object.entries(cifra)) {
+  console.log(decifrar(decifrar, key));
+  console.log(key, value);
+}
+let texto = decifrar;
+
+console.log();
+for (const [key, value] of Object.entries(cifra)) {
+  texto = texto.replace(key, value);
+  console.log(texto);
+}
